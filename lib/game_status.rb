11b1416e8a08eb7bf3d1a77988837a3afab774a5
Phuong Vu <phuong.pmv@gmail.com>
2017-10-13 07:53:45 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,7 +21,7 @@ def won?(board)
     win_index_2 = win_combo[1]
     win_index_3 = win_combo[3]
 
-    if position_taken?[board, win_index_1]
+    if position_taken?(board, win_index_1)
       position_1 = board[win_index_1]
       if position_1 == board[win_index_2] && position_1 == board[win_index_3]
         return win_combo
